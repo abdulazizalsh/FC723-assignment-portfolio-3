41351c82e16f1faf29f2ed287b954841307125f4
abdulazizalsh <abdulazizalshammariii@gmail.com>
2026-05-12 12:28:02 +0100

    Created project structure
    FC723 scientific calculator project built with Python Tkinter.

---
+++ Application/Main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 12 12:10:28 2026
+
+@author: abdul
+"""
+
+gh
---
+++ Application/calculator logic.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 12 12:17:45 2026
+
+@author: abdul
+"""
+
+gh
